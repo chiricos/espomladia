a7206affdc5affe51ddf9e943bf7fbec1b47bacf
chiricos <edwarddiaz92@gmail.com>
2016-04-01 17:14:11 -0500

    agregando nuevos banners

--- js/script.js
+++ js/script.js
@@ -8,4 +8,21 @@ $(window).load(function() {
             direction: "horizontal",
             animation: "slide",
         });
+    $('.banner-back').flexslider(
+        {
+            controlNav: true,               //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
+            directionNav: true,             //Boolean: Create navigation for previous/next navigation? (true/false)
+            prevText: "Previous",           //String: Set the text for the "previous" directionNav item
+            nextText: "Next",
+            direction: "horizontal",
+            animation: "slide",
+        });
+    $('.banner-front').flexslider(
+        {
+            controlNav: false,               //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
+            directionNav: true,             //Boolean: Create navigation for previous/next navigation? (true/false)
+            prevText: "Previous",           //String: Set the text for the "previous" directionNav item
+            nextText: "Next",
+            direction: "horizontal",
+        });
 });
