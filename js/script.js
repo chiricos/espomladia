9e44f3719b6b9d162d9b77fb99fd827f3b97a5a7
chiricos <edwarddiaz92@gmail.com>
2016-04-01 18:05:32 -0500

    fdksa

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ $(window).load(function() {
         });
     $('.banner-back').flexslider(
         {
-            controlNav: true,               //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
+            controlNav: false,               //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
             directionNav: true,             //Boolean: Create navigation for previous/next navigation? (true/false)
             prevText: "Previous",           //String: Set the text for the "previous" directionNav item
             nextText: "Next",
